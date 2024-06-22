c27af0c7668708b9de810e30df0bf8e5ce8e5785
Al13n0231 <139857884+Al13n0231@users.noreply.github.com>
2024-06-22 16:04:51 -0400

    updated the GameTheory with a new strategy

--- src/com/allen/GameTheory/GameTheory.java
+++ src/com/allen/GameTheory/GameTheory.java
@@ -1,18 +1,26 @@
 package com.allen.GameTheory;
 
-public class GameTheory { 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-		Player player1 = new Player(20);
-		Player player2 = new Player(20);
-		player1.getAttacked();
-		if (player1.getPoints() < 20){
-			player2.getAttacked();
-			
-		}
-		System.out.println("player1: " + player1.getPoints());
-		System.out.println("player2: " + player2.getPoints());
-	}
-		
-	
-}
+public class GameTheory {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Player player1 = new Player(20);
+        Player player2 = new Player(20);
+
+        // Implementing the strategy
+        for (int i = 0; i < 5; i++) { // Example loop for 3 rounds
+            player2.getAttacked(); // Player1 attacks Player2
+
+            if (player2.getAttackCounter() % 3 == 0) {
+                player1.getAttacked();
+                player1.attack();// Player2 taken 1 point on the third attack
+            }else {player1.attack(); // Player2 taken 1 point for attacking
+            
+            }
+        
+        }
+        System.out.println("player1: " + player1.getPoints());
+        System.out.println("player2: " + player2.getPoints());
+        System.out.println("attackcounter1: " + player1.getAttackCounter());
+        System.out.println("attackcounter2: " + player2.getAttackCounter());
+    }}
+       
--- src/com/allen/GameTheory/Player.java
+++ src/com/allen/GameTheory/Player.java
@@ -1,23 +1,32 @@
 package com.allen.GameTheory;
 
 public class Player {
-	//start with 20 points
-	
-	private int points;
-	
+    private int points;
+    private int attackCounter; // Counter to track the number of attacks
 
-	//function1 attack: 
-	//function2 gets attacked: lose point -> attack/wait/nothing
-	public Player(int points) {
+    public Player(int points) {
         this.points = points;
-        
+        this.attackCounter = 0; // Initialize the attack counter
     }
-	public void getAttacked() {
-		points -= 1;
-	}
-	
-	public int getPoints() {
-		return points;
-	}
 
+    public void getAttacked() {
+        points -= 1;
+        attackCounter += 1; // Increment the attack counter when attacked
+    }
+
+    public void attack() {
+        points -= 1; // Decrease points when attacking
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getAttackCounter() {
+        return attackCounter;
+    }
+
+    public void resetAttackCounter() {
+        attackCounter = 0; // Reset the attack counter
+    }
 }
